1692d0c7421b970426ed1010c22647661c7cac56
Luke Plant <L.Plant.98@cantab.net>
2017-10-06 22:56:41 +0300

    Attempt to workaround coveralls flakiness

--- travis_tests.sh
+++ travis_tests.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-coverage run --branch --source=parsy `which py.test` && coveralls
+coverage run --branch --source=parsy `which py.test` || exit 1
+
+# Coveralls is flaky sometimes, especially for concurrent uploads.
+# https://github.com/lemurheavy/coveralls-public/issues/487
+# So try again if it fails first time.
+coveralls || { sleep $((RANDOM / 4000 + 1)); coveralls }
