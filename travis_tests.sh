24b5b9ff9827da341b2e3b2744e82ca2b0cbe6f6
Luke Plant <L.Plant.98@cantab.net>
2017-10-07 19:52:12 +0300

    Fixed bash script

--- travis_tests.sh
+++ travis_tests.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 coverage run --branch --source=parsy `which py.test` || exit 1
 
 # Coveralls is flaky sometimes, especially for concurrent uploads.
 # https://github.com/lemurheavy/coveralls-public/issues/487
 # So try again if it fails first time.
-coveralls || { sleep $((RANDOM / 4000 + 1)); coveralls }
+coveralls || { sleep $((RANDOM / 4000 + 1)); coveralls; }
