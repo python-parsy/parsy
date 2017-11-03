c5c0f55dfe40afd1865dece47c86ff5ebd86d3aa
Luke Plant <L.Plant.98@cantab.net>
2017-11-03 18:34:06 +0300

    codecov integration

--- travis_tests.sh
+++ travis_tests.sh
@@ -2,6 +2,8 @@
 
 coverage run --branch --source=parsy `which py.test` || exit 1
 
+codecov
+
 # Coveralls is flaky sometimes, especially for concurrent uploads.
 # https://github.com/lemurheavy/coveralls-public/issues/487
 # So try again if it fails first time.
