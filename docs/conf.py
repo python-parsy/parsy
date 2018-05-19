898ced7493366fa94877cad41d2882485a264f6f
Luke Plant <L.Plant.98@cantab.net>
2018-05-19 10:18:41 +0300

    Re-added 'src' to sys.path in sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,11 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../src"))
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
